0cf13c1d090fe9246b23f47d04e4be9e86509163
cortezd334 <cortezd334@gmail.com>
2020-08-20 10:17:17 -0700

    show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,23 @@ class UsersController < ApplicationController
         render json: user
     end
 
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def login
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: user
+        else
+            render json: {error: "Incorrect username and/or password"}
+        end
+    end
+
+    private
     def user_params
-        params.require(:user).permit(:name, :age, :address)
+        params.permit(:name, :age, :address, :username, :password, :party)
         # do we need to add anything else in the permits?
     end
 end
